4317230e5722c87c2338b229e62d43884d14fd9d
vader9595 <102444679+vader9595@users.noreply.github.com>
2022-08-23 09:49:01 -0500

    Initial commit

---
+++ src/main/java/com/example/SpringBoot_402/SpringBoot402Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_402;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot402Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot402Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBoot_402/SpringBoot402ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_402;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot402ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
